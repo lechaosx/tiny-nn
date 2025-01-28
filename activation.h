bc65fbb38d54075c847f13030569c0e889ab4e05
Drahomír Dlabaja <lechaosx@gmail.com>
2025-01-28 22:38:06 +0100

    Use span

--- activation.h
+++ activation.h
@@ -1,37 +1,38 @@
 #include <cmath>
-#include <algorithm>
 
-inline Eigen::MatrixXf sigmoidActivation(const Eigen::MatrixXf& x) {
+#include <Eigen/Core>
+
+inline constexpr Eigen::MatrixXf sigmoidActivation(const Eigen::MatrixXf& x) {
 	return x.unaryExpr([](float val) { return 1.0f / (1.0f + std::exp(-val)); });
 }
 
-inline Eigen::MatrixXf sigmoidDerivative(const Eigen::MatrixXf& x) {
+inline constexpr Eigen::MatrixXf sigmoidDerivative(const Eigen::MatrixXf& x) {
 	return x.unaryExpr([](float val) {
 		float s = 1.0f / (1.0f + std::exp(-val));
 		return s * (1.0f - s);
 	});
 }
 
-inline Eigen::MatrixXf tanhActivation(const Eigen::MatrixXf& x) {
+inline constexpr Eigen::MatrixXf tanhActivation(const Eigen::MatrixXf& x) {
 	return x.unaryExpr([](float val) { return std::tanh(val); });
 }
 
-inline Eigen::MatrixXf tanhDerivative(const Eigen::MatrixXf& x) {
+inline constexpr Eigen::MatrixXf tanhDerivative(const Eigen::MatrixXf& x) {
 	return x.unaryExpr([](float val) { 
 		float t = std::tanh(val);
 		return 1.0f - t * t; 
 	});
 }
 
-inline Eigen::MatrixXf reluActivation(const Eigen::MatrixXf& x) {
+inline constexpr Eigen::MatrixXf reluActivation(const Eigen::MatrixXf& x) {
 	return x.unaryExpr([](float val) { return std::max(0.0f, val); });
 }
 
-inline Eigen::MatrixXf reluDerivative(const Eigen::MatrixXf& x) {
+inline constexpr Eigen::MatrixXf reluDerivative(const Eigen::MatrixXf& x) {
 	return x.unaryExpr([](float val) { return val > 0 ? 1.0f : 0.0f; });
 }
 
-inline Eigen::MatrixXf softmaxActivation(const Eigen::MatrixXf& x) {
+inline constexpr Eigen::MatrixXf softmaxActivation(const Eigen::MatrixXf& x) {
 	Eigen::MatrixXf expVals = (x.rowwise() - x.colwise().maxCoeff()).array().exp();
 	return expVals.array().rowwise() / (expVals.colwise().sum()).array();
 }
--- main.cpp
+++ main.cpp
@@ -1,43 +1,49 @@
 #include <print>
 #include <vector>
+#include <span>
+#include <array>
 #include <ranges>
 #include <functional>
 
-#include <Eigen/Dense>
+#include <Eigen/Core>
 
 #include "activation.h"
+
+struct Activation {
+	std::function<Eigen::MatrixXf(const Eigen::MatrixXf &)> activation;
+	std::function<Eigen::MatrixXf(const Eigen::MatrixXf &)> derivative;
+};
+
+const Activation Sigmoid { sigmoidActivation, sigmoidDerivative };
+const Activation Tanh { tanhActivation, tanhDerivative };
+const Activation Relu { reluActivation, reluDerivative };
 
 struct Layer {
 	Eigen::MatrixXf weights;
 	Eigen::VectorXf biases;
 
-	struct Activation {
-		std::function<Eigen::MatrixXf(const Eigen::MatrixXf &)> activation;
-		std::function<Eigen::MatrixXf(const Eigen::MatrixXf &)> derivative;
-	} activation;
+	Activation activation;
 };
 
-const Layer::Activation Sigmoid { sigmoidActivation, sigmoidDerivative };
-const Layer::Activation Tanh { tanhActivation, tanhDerivative };
-const Layer::Activation Relu { reluActivation, reluDerivative };
-
-
-std::vector<Layer> createNeuralNetwork(const std::vector<uint64_t> &topology, const Layer::Activation &activation) {
+template <size_t N>
+std::vector<Layer> constexpr createNeuralNetwork(std::span<const uint64_t, N + 1> topology, std::span<const Activation, N> activations) {
 	std::vector<Layer> nn{};
 
-	for (size_t i = 0; i < std::size(topology) - 1; ++i) {
-		nn.emplace_back(Eigen::MatrixXf::Random(topology[i + 1], topology[i]), Eigen::VectorXf::Random(topology[i + 1]), activation);
+	for (size_t i = 0; i < N; ++i) {
+		nn.emplace_back(Eigen::MatrixXf::Random(topology[i + 1], topology[i]), Eigen::VectorXf::Random(topology[i + 1]), activations[i]);
 	}
 
 	return nn;
 }
 
-Eigen::MatrixXf feedLayer(const std::vector<Layer> &nn, const Eigen::MatrixXf &inputs, size_t layer = 0) {
-	if (layer == std::size(nn)) {
+Eigen::MatrixXf constexpr feedLayer(std::span<const Layer> nn, const Eigen::MatrixXf &inputs) {
+	if (nn.empty()) {
 		return inputs;
 	}
 
-	return feedLayer(nn, nn[layer].activation.activation((nn[layer].weights * inputs).colwise() + nn[layer].biases), layer + 1);
+	const Layer &layer = nn.front();
+
+	return feedLayer(nn.subspan(1), layer.activation.activation((layer.weights * inputs).colwise() + layer.biases));
 }
 
 template<typename F>
@@ -46,23 +52,25 @@ concept LossDerivative = requires(const F &f, const Eigen::MatrixXf &outputs) {
 };
 
 template <LossDerivative F>
-Eigen::MatrixXf trainLayer(std::vector<Layer> &nn, const Eigen::MatrixXf &inputs, float learningRate, const F &lossDerivative, size_t layer = 0) {
-	if (layer == std::size(nn)) {
+Eigen::MatrixXf constexpr trainLayer(std::span<Layer> nn, const Eigen::MatrixXf &inputs, float learningRate, const F &lossDerivative) {
+	if (nn.empty()) {
 		return lossDerivative(inputs);
 	}
 
-	Eigen::MatrixXf zs = (nn[layer].weights * inputs).colwise() + nn[layer].biases;
+	Layer &layer = nn.front();
 
-	Eigen::MatrixXf delta = trainLayer(nn, nn[layer].activation.activation(zs), learningRate, lossDerivative, layer + 1).cwiseProduct(nn[layer].activation.derivative(zs));
+	Eigen::MatrixXf zs = (layer.weights * inputs).colwise() + layer.biases;
 
-	nn[layer].weights -= learningRate * delta * inputs.transpose();
-	nn[layer].biases -= learningRate * delta.rowwise().sum();
+	Eigen::MatrixXf delta = trainLayer(nn.subspan(1), layer.activation.activation(zs), learningRate, lossDerivative).cwiseProduct(layer.activation.derivative(zs));
 
-	return nn[layer].weights.transpose() * delta;
+	layer.weights -= learningRate * delta * inputs.transpose();
+	layer.biases -= learningRate * delta.rowwise().sum();
+
+	return layer.weights.transpose() * delta;
 }
 
 template <LossDerivative F>
-void trainNeuralNetwork(std::vector<Layer> &nn, const Eigen::MatrixXf& input, float learningRate, const F &lossDerivative) {
+void constexpr trainNeuralNetwork(std::span<Layer> nn, const Eigen::MatrixXf& input, float learningRate, const F &lossDerivative) {
 	std::vector<Eigen::MatrixXf> activations, zs;
 
 	activations.push_back(input);
@@ -88,7 +96,7 @@ void trainNeuralNetwork(std::vector<Layer> &nn, const Eigen::MatrixXf& input, fl
 }
 
 int main() {
-	std::vector<Layer> nn = createNeuralNetwork({2, 3, 3}, Sigmoid);
+	std::vector<Layer> nn = createNeuralNetwork<2>(std::array<uint64_t, 3>{2, 3, 3}, std::array<Activation, 2>{Sigmoid, Sigmoid});
 
 	Eigen::MatrixXf inputs(2, 4);
 	inputs << 0, 0, 1, 1,
